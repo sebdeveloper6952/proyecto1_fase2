0427a098e78cc6af2d2482495c5db92f1f3f0481
JoseArenas <jo.arenas96@gmail.com>
2018-03-17 05:09:53 +0000

    comentarios a funciones

--- ADT proyecto.c
+++ ADT proyecto.c
@@ -1,12 +1,20 @@
+//---------- LIBRERIAS IMPORTADAS----------------------------------
+
 #include "simpletools.h"
 #include "abdrive.h"
 #include "ping.h"
+
+
+
+//---------- ESPACIO DE VARIABLES---------------------------------
 
 int front_s_pin = 8;
 int right_s_pin = 7;
 int turns = 0;
 int dist = 15;
 int move_dist = 32;
+
+//----------- CODIGO PRINCIPAL -----------------------------------
 
 int main()
 {
@@ -17,16 +25,20 @@ int main()
     while(ping_cm(front_s_pin) > dist) { pause(5); }
     drive_ramp(0,0);
     
+    //al llegar a una pared, cruza a la izquierda para manterner la pared siempre a su derecha, y entra al loop principal 
     cruzar_izquierda();
     
+    //El robot entrara a este loop, si el contador de las vueltas no es cero. En caso sea cero, esto indica que ya supero el obstaculo y sale del loop.
     while(turns != 0)
     {
-      // ver si hay pared a la derecha
+      // ver si la pared que se encuentra a su derecha esta a una distancia mayor de la maxima tolerada. 
       if(ping_cm(right_s_pin) > dist)
       {
-        // cruzar a la derecha y avanzar una revolucion
+        // cruzar a la derecha, y avanza revolucion y media si la distancia lo permite.
         cruzar_derecha();
         if(ping_cm(front_s_pin > 110)) { drive_goto(96,96); } 
+        
+        //si la distancia es menor, avanza solo 20 ticks de las ruedas para no chocar con ningun obstaculo.. 
         else
         {
           drive_goto(20,20);
@@ -43,29 +55,22 @@ int main()
     
       // avanzar una revolucion
       drive_goto(move_dist, move_dist);        
-
-  //drive_ramp(0,0);
     }
   }    
 }
 
+//mueve las ruedas del robot 27 ticks en sentidos opuestos para lograr que el robot cruce a la izquierda. Le resta al contador para poder hacer cumplir la condicion del loop.
 void cruzar_izquierda()
 {
   drive_goto(-27,27);
-  //drive_speed(-38.5,38);
-  //pause(370);
-  
   turns--;
 }
 
+
+//mueve las ruedas del robot 27 ticks en sentidos opuestos para lograr que el robot cruce a la derecha. Le suma al contador para poder hacer cumplir la condicion del loop.
 void cruzar_derecha()
 {
   drive_goto(27,-27);
-  
-  //drive_ramp(0,0);
-  //drive_speed(38.5,-38);
-  //pause(370);
-  
   turns++;
 } 
 
